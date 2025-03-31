cf8d5dfc93f53e7ee5357fbf492b0fed37ccaa05
realendie <enderproplays54@gmail.com>
2025-03-30 22:55:55 -0400

    Add index view to URL patterns

--- appointment_tracker/urls.py
+++ appointment_tracker/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from appointment_tracker import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', views.index)
 ]
